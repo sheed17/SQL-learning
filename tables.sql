c14c227d249333b463f933d827e0961694eef481
sheed17 <rsamady2@gmail.com>
2025-09-10 23:57:54 -0700

    added tables reference

---
+++ tables.sql
@@ -0,0 +1,32 @@
+##### TABLES and actions #####
+/*CREATE TABLE Employees(
+	employee_id INT,
+    first_name VARCHAR(50),
+    last_name VARCHAR(50),
+    hourly_pay DECIMAL(5, 2),
+    hire_date DATE
+
+
+
+);*/
+
+#SELECT * FROM Employees
+
+#RENAME TABLE Employees TO workers;
+#RENAME TABLE workers TO employees;
+
+
+/*ALTER TABLE employees
+ADD phone_number VARCHAR(15);*/
+
+/*ALTER TABLE employees
+RENAME COLUMN phone_number to email*/
+
+/*ALTER TABLE employees
+MODIFY COLUMN email VARCHAR(100);*/
+
+/*ALTER TABLE employees
+MODIFY email VARCHAR(100)
+AFTER last_name*/
+
+##### TABLES and actions #####
