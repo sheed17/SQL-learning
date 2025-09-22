608d1ef6910a26ee714f695e582629d2dcb86664
sheed17 <rsamady2@gmail.com>
2025-09-22 00:12:39 -0700

    Create order_by.sql

---
+++ basics/order_by.sql
@@ -0,0 +1,8 @@
+#ORDER BY- puts columns in order of ascending or descending for strings it is alphabetical
+# for numbers in that order from least to most and desc makes it most to least
+
+SELECT * FROM employees
+ORDER BY first_name DESC;
+
+SELECT * FROM transactions
+ORDER BY amount;
