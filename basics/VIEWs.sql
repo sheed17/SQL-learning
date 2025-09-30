97df04f6739c4d96772c5b7374eef7ccf22099f1
sheed17 <rsamady2@gmail.com>
2025-09-30 10:40:52 -0700

    Update VIEWs.sql

--- basics/VIEWs.sql
+++ basics/VIEWs.sql
@@ -1,14 +1,14 @@
 #Views - virtual table that can interacted with they are not real tables but are in a field that can be viewed
 
-/*CREATE VIEW employee_attendance AS SELECT
-first_name, last_name FROM employees;*/
+CREATE VIEW employee_attendance AS SELECT
+first_name, last_name FROM employees;
 
-/*CREATE VIEW employee_email AS SELECT
-email FROM employees;*/
+CREATE VIEW employee_email AS SELECT
+email FROM employees;
 
-/*
+
 UPDATE employees
 SET email = 'bobjohn@yahoo.com'
-WHERE employee_id = 10;*/ # Even after updating a value the VIEW is also updated
+WHERE employee_id = 10; # Even after updating a value the VIEW is also updated
 
 SELECT * FROM employee_email;
